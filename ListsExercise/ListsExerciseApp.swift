fe6d3471643041ebd990c79d701b3c1fb580328f
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-05-17 10:48:00 -0500

    Initial Commit

---
+++ ListsExercise/ListsExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsExerciseApp.swift
+//  ListsExercise
+//
+//  Created by Ashlyn Barrera on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ListsExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
